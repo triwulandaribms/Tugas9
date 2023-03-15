601ca82df00f5e815474e94a59cd779374c32193
Tri Wulandari <triwulandari3456@gamil.com>
2023-03-15 22:54:51 +0700

    add path

--- api/index.js
+++ api/index.js
@@ -4,11 +4,17 @@ import { client } from "./db.js";
 
 const app = express();
 
+const data = ["Tri Wulandari", "D3 Manajemen informatika"];
 app.use(express.static("public"));
 
 app.get("/api/planet", async (_req, res) => {
   const results = await client.query("SELECT * FROM planet");
   res.send(results.rows);
+});
+
+app.get("/api/data", async (req, res) => {
+  console.log(`data mahasiswa ${data}`);
+  res.send(data);
 });
 
 app.listen(3000, () => {
